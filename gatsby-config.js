c59365c30863b5cd2ebb69fe9f7b50facaa546a1
Sam Richards <sbrichards@gmail.com>
2020-02-08 19:58:37 -0800

    Update font to Ubuntu

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Ubuntu`,
+            subsets: [`latin`],
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
